2854d5b37fb0b2428a16b65fe51e2789f54df98d
baris-inandi <68742481+baris-inandi@users.noreply.github.com>
2022-09-14 15:09:57 +0300

    init

---
+++ Apples.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Apples {
+    public static void main(String[] args) {
+        List<Integer> appleWeights = Arrays.asList(200, 300, 420, 129, 482, 847, 182, 123, 495, 301);
+        int price = 2;
+        int weight = 0;
+        for (int i = 0; weight < 2000; i++) {
+            weight += appleWeights.get(i);
+        }
+        System.out.println("Pay " + weight * price + "TL for " + weight + " grams of apples.");
+    }
+}
---
+++ Fib.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Fib {
+    public static List<Integer> fib(int threshold) {
+        if (threshold <= 0) {
+            return Collections.emptyList();
+        }
+        List<Integer> initial = Arrays.asList(1, 1);
+        if (threshold == 1) {
+            return initial;
+        }
+        List<Integer> l = new ArrayList<>(initial);
+        while (l.get(l.size() - 1) <= threshold) {
+            l.add(l.get(l.size() - 1) + l.get(l.size() - 2));
+        }
+        return l;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(fib(10 ^ 1000000000));
+    }
+}
