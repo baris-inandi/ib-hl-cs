eece3c184d2b4035907d1b4c8294f040b98a567c
baris-inandi <68742481+baris-inandi@users.noreply.github.com>
2022-10-31 19:21:56 +0300

    commit

---
+++ exam_study/Diamond.java
@@ -0,0 +1,24 @@
+package exam_study;
+
+class Diamond {
+    public static void main(String args[]) {
+        int size = 7, center = 1, numberOfSpace = size / 2;
+        for (int i = 1; i <= size; i++) {
+            for (int k = numberOfSpace; k >= 1; k--) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= center; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+            if (i < size / 2 + 1) {
+                center += 2;
+                numberOfSpace -= 1;
+            } else {
+                center -= 2;
+                numberOfSpace += 1;
+
+            }
+        }
+    }
+}
---
+++ exam_study/Populate.java
@@ -0,0 +1,13 @@
+package exam_study;
+
+public class Populate {
+    public static void main(String[] args) {
+        // employees: [surname, name, surname, name, surname, name]yapa
+        String[] employees = new String[1200];
+        String[] surnames = new String[600];
+
+        for (int i = 0; i <= surnames.length; i++) {
+            surnames[i] = employees[2 * i];
+        }
+    }
+}
